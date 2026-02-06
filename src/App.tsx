91ffa4f59c76cc8b39077f75557dbdb5535b0d90
OutsydBaby <amichdot@gmail.com>
2026-02-06 13:20:11 +0100

    Added AuthPage & routing from react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./assets/LandingPage";
+import AuthPage from "./Pages/Auth";
 
 const App = () => {
   return (
-    <>
-      <LandingPage />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/auth" element={<AuthPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
