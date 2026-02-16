6776294617f9596cb62ada81a4939483d0525334
OutsydBaby <amichdot@gmail.com>
2026-02-16 11:46:43 +0100

    Added routing to the chatPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./assets/LandingPage";
 import AuthPage from "./Pages/Auth";
+import ChatPage from "./Pages/ChatPage";
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/auth" element={<AuthPage />} />
+        <Route path="/chat" element={<ChatPage />} />
       </Routes>
     </BrowserRouter>
   );
