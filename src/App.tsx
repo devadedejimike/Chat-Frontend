d5411f17cbf0055db95d3418bfedd4a23b6ce46a
OutsydBaby <amichdot@gmail.com>
2026-02-24 15:32:16 +0100

    refactor: update App routing structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./assets/LandingPage";
 import AuthPage from "./Pages/Auth";
-import ChatPage from "./Pages/ChatPage";
+import Chat from "./Pages/ChatPage";
 
 const App = () => {
   return (
@@ -9,7 +9,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/auth" element={<AuthPage />} />
-        <Route path="/chat" element={<ChatPage />} />
+        <Route path="/chat" element={<Chat />} />
       </Routes>
     </BrowserRouter>
   );
